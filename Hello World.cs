853470debcdaa0c3166a16f92ecd325f7edadb82
Dmitriy Yakovlev <cinhrofazatron8@yandex.ru>
2019-09-20 14:21:34 +0300

    constructor

--- Hello World.cs
+++ Hello World.cs
@@ -1,8 +1,21 @@
 
 class MyClass{
-    public statitic void main(String[] args){
+    
+    private string name {get; set};
+    public static int numberOfObjects;
+
+    public static int Main(string[] args)
+    {
         Console.WriteLine('Hello World!!!');
         Console.ReadLine();
         return;
+        return 0;
     }
+
+    private MyClass(){
+        this.numberOfObjects += 1;
+        return;
+    }
+
 }
+
