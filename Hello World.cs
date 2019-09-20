0c3eace4bfe0cc019f26a97ee9ce0308963b8a61
Dmitriy Yakovlev <cinhrofazatron8@yandex.ru>
2019-09-20 14:26:28 +0300

    add version

--- Hello World.cs
+++ Hello World.cs
@@ -1,8 +1,12 @@
 
+//version 1.1
+
 class MyClass{
     public statitic void main(String[] args){
         Console.WriteLine('Hello World!!!');
         Console.ReadLine();
         return;
     }
+
+
 }
